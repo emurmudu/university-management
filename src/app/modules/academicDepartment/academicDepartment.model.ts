3bf98561191d92a7df636b05a8aa6883b61ffe15
emurmudu <eliasmurmudu@gmail.com>
2024-05-31 23:09:43 +0600

    error handling when updating a deleted id/ null id

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -17,7 +17,7 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>({
     timestamps: true,
 })
 
-
+// same academic department name error handling
 academicDepartmentSchema.pre('save', async function(next){
     const isDepartmentExists = await AcademicDepartment.findOne({
         name : this.name,
@@ -29,5 +29,22 @@ academicDepartmentSchema.pre('save', async function(next){
     next();
 
 })
+
+// error handling when updating a deleted id/ null id
+academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
+
+    const query = this.getQuery();
+
+    const isDepartmentExists = await AcademicDepartment.findOne(query)
+
+    if(!isDepartmentExists){
+        throw new Error('This department does not exist')
+    }
+
+    next();
+
+})
+
+
 
 export const AcademicDepartment = model<TAcademicDepartment>('AcademicDepartment', academicDepartmentSchema)
