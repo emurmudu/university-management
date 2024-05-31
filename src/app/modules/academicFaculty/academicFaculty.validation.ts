fe76122066a0a8e73301390a66b078c6b8357ad3
emurmudu <eliasmurmudu@gmail.com>
2024-05-31 13:56:29 +0600

    academic faculty controller done

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,11 +1,21 @@
 import { z } from "zod";
 
-const academicFacultyValidationSchema = z.object({
+const createAcademicFacultyValidationSchema = z.object({
     name: z.string({
         invalid_type_error: 'Academic faculty must be string',
     }),
 });
 
+const updateAcademicFacultyValidationSchema = z.object({
+    name: z.string({
+        invalid_type_error: 'Academic faculty must be string',
+    }),
+});
+
+
+
+
 export const AcademicFacultyValidations ={
-    academicFacultyValidationSchema
+    createAcademicFacultyValidationSchema,
+    updateAcademicFacultyValidationSchema,
 }
