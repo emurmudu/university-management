416f3553b870bc4ddd634b7c3e27659ea5b2705e
emurmudu <eliasmurmudu@gmail.com>
2024-05-31 23:30:07 +0600

    populate referencing field

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -17,7 +17,7 @@ const createAcademicDepartmentIntoDB = async(payload:TAcademicDepartment)=>{
 
 
 const getAllAcademicDepartmentFromDB = async()=>{
-    const result = await AcademicDepartment.find();
+    const result = await AcademicDepartment.find().populate('academicFaculty');
     return result;
 }
 
