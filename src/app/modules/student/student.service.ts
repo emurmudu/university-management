c9924eb216b949aa3ef84f5563fbcefcffb87052
emurmudu <eliasmurmudu@gmail.com>
2024-06-01 14:40:14 +0600

    primitive & non-primitive data dynamic update done

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -36,9 +36,45 @@ const getSingleStudentFromDB = async (id: string) => {
 
 
 const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
+  const {name, guardian, localGuardian, ...remainingStudentData} = payload;
 
+  const modifiedUpdatedData : Record<string, unknown>={
+    ...remainingStudentData
+  }
 
-  const result = await StudentModel.findOneAndUpdate({ id }, payload);
+  /*
+    guardian : {
+       fatherOccupation : "Teacher"
+    }
+    
+    guardina.fatherOccupation = "Teacher"
+    
+   */
+  if(name && Object.keys(name).length){
+    for(const [key, value] of Object.entries(name)){
+      modifiedUpdatedData [`name.${key}`] = value;
+    }
+  }
+
+  if(guardian && Object.keys(guardian).length){
+    for(const [key, value] of Object.entries(guardian)){
+      modifiedUpdatedData [`guardian.${key}`] = value;
+    }
+  }
+
+  if(localGuardian && Object.keys(localGuardian).length){
+    for(const [key, value] of Object.entries(localGuardian)){
+      modifiedUpdatedData [`localGuardian.${key}`] = value;
+    }
+  }
+
+  console.log(modifiedUpdatedData);
+
+  const result = await StudentModel.findOneAndUpdate(
+    { id }, 
+    modifiedUpdatedData,
+    {new:true, runValidators:true}
+  );
   return result;
 };
 
@@ -73,6 +109,8 @@ const deleteStudentFromDB = async (id: string) => {
   } catch (error) {
     await session.abortTransaction();
     await session.endSession();
+
+    throw new AppError(httpStatus.BAD_REQUEST,'Failed to delete student')
   }
 };
 
